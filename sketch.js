12f643b79fb10743cd6a7170d23b18eb964287f0
Valentina Fava <valentinafava93@gmail.com>
2025-02-05 21:09:31 -0300

    Add interactivity to dropdown and pin image

--- sketch.js
+++ sketch.js
@@ -1,24 +1,180 @@
-let img;
+let img1, img2;
+let pinCoordinates = [];
 
 function preload() {
-  img = loadImage('assets/FiveCountries.png');
+  img1 = loadImage('assets/FiveCountries.png');
+  img2 = loadImage('assets/pin.png');
 }
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  dropdown = select('#country-select');
+  dropdown.changed(handleDropdownChange);
 }
+
+
+function handleDropdownChange() {
+  selectedOption = int(dropdown.value());
+  console.log("Selected option:", selectedOption);
+  selectCountries();
+}
+
+
+function selectCountries() {
+  switch (selectedOption) {
+    case 0:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 1:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 2:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 3:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 4:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 5:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 6:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 7:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 8:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 9:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 10:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 11:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 12:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 13:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    case 14:
+      pinCoordinates = [
+        {x: 100, y: 150},
+        {x: 200, y: 250},
+        {x: 300, y: 350},
+        {x: 400, y: 450},
+        {x: 500, y: 550}
+      ];
+      break;
+    // Add more cases for other options
+    default:
+      pinCoordinates = [];
+      break;
+  }
+}
+
 
 function draw() {
   background(255);
 
-  // Calculate the scale factor
-  let imgAspect = img.width / img.height;
+  let imgAspect = img1.width / img1.height;
   let canvasAspect = width / height;
     
   let imgWidth, imgHeight;
     
-  // Maintain the aspect ratio
   if (imgAspect > canvasAspect) {
     imgWidth = width;
     imgHeight = width / imgAspect;
@@ -27,10 +183,13 @@ function draw() {
     imgWidth = height * imgAspect;
   }
     
-  // Center the image on the canvas
   let x = (width - imgWidth) / 2;
   let y = (height - imgHeight) / 2;
   
-  // Draw the image, adjusted to fit the canvas without distortion
-  image(img, x, y, imgWidth, imgHeight);
+  image(img1, x, y, imgWidth, imgHeight);
+
+  for (let i = 0; i < pinCoordinates.length; i++) {
+    let pin = pinCoordinates[i];
+    image(img2, pin.x, pin.y, 14.3, 30);
+  }
 }
