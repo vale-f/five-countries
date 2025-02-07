9e3e4b8565a2c2807a6298421f3ace354401730a
Valentina Fava <valentinafava93@gmail.com>
2025-02-07 14:32:39 -0300

    Adjusted pins' coordinates and added sources' texts.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let img1, img2;
 let pinCoordinates = [];
+let source;
 
 function preload() {
   img1 = loadImage('assets/FiveCountries.png');
@@ -32,15 +33,17 @@ function selectCountries() {
         {x: 1420, y: 572},
         {x: 1224, y: 392}
       ];
+      source = "Source: World Bank. https://data.worldbank.org/indicator/SP.POP.TOTL?most_recent_value_desc=true";
       break;
     case 1:
       pinCoordinates = [
         {x: 978, y: 323},
-        {x: 1637, y: 540},
-        {x: 1677, y: 571},
-        {x: 1528, y: 499},
-        {x: 981, y: 312}
+        {x: 270, y: 615},
+        {x: 1637, y: 573},
+        {x: 699, y: 794},
+        {x: 640, y: 460}
       ];
+      source = "Source: World Population Review. https://worldpopulationreview.com/country-rankings/smallest-countries";
       break;
     case 2:
       pinCoordinates = [
@@ -50,15 +53,17 @@ function selectCountries() {
         {x: 1428, y: 418},
         {x: 903, y: 349}
       ];
+      source = "Source: Our World in Data. https://ourworldindata.org/most-densely-populated-countries";
       break;
     case 3:
       pinCoordinates = [
         {x: 805, y: 198},
-        {x: 700, y: 797},
+        {x: 699, y: 794},
         {x: 863, y: 410},
         {x: 1335, y: 300},
         {x: 995, y: 650}
       ];
+      source = "Source: Global Citzen Soultions. https://www.globalcitizensolutions.com/countries-with-lowest-population-density/";
       break;
     case 4:
       pinCoordinates = [
@@ -68,15 +73,17 @@ function selectCountries() {
         {x: 970, y: 285},
         {x: 922, y: 272}
       ];
+      source = "Source: Wikipedia. https://en.wikipedia.org/wiki/List_of_countries_by_total_wealth#cite_note-databook2023-2";
       break;
     case 5:
       pinCoordinates = [
-        {x: 100, y: 150},
-        {x: 200, y: 250},
-        {x: 300, y: 350},
-        {x: 400, y: 450},
-        {x: 500, y: 550}
+        {x: 952, y: 537},
+        {x: 673, y: 509},
+        {x: 1050, y: 681},
+        {x: 853, y: 472},
+        {x: 1108, y: 478}
       ];
+      source = "Source: Wikipedia. https://en.wikipedia.org/wiki/List_of_countries_by_total_wealth#cite_note-databook2023-2";
       break;
     case 6:
       pinCoordinates = [
@@ -84,8 +91,9 @@ function selectCountries() {
         {x: 1390, y: 528},
         {x: 1422, y: 421},
         {x: 898, y: 268},
-        {x: 1167, y: 408}
+        {x: 1174, y: 402}
       ];
+      source = "Source: World Bank. https://data.worldbank.org/indicator/NY.GDP.PCAP.PP.CD?most_recent_value_desc=true";
       break;
     case 7:
       pinCoordinates = [
@@ -95,6 +103,7 @@ function selectCountries() {
         {x: 1125, y: 520},
         {x: 1023, y: 556}
       ];
+      source = "Source: World Bank. https://data.worldbank.org/indicator/NY.GDP.PCAP.PP.CD?most_recent_value_desc=true";
       break;
     case 8:
       pinCoordinates = [
@@ -104,6 +113,7 @@ function selectCountries() {
         {x: 1040, y: 275},
         {x: 1040, y: 299}
       ];
+      source = "Source: https://data.worldbank.org/indicator/SI.POV.GINI?most_recent_value_desc=false";
       break;
     case 9:
       pinCoordinates = [
@@ -113,6 +123,7 @@ function selectCountries() {
         {x: 1065, y: 668},
         {x: 1024, y: 650}
       ];
+      source = "Source: https://data.worldbank.org/indicator/SI.POV.GINI?most_recent_value_desc=false";
       break;
     case 10:
       pinCoordinates = [
@@ -122,6 +133,7 @@ function selectCountries() {
         {x: 1270, y: 435},
         {x: 500, y: 352}
       ];
+      source = "Source: We Forum. https://www.weforum.org/stories/2021/03/these-are-the-top-ten-countries-for-linguistic-diversity/";
       break;
     case 11:
       pinCoordinates = [
@@ -131,6 +143,7 @@ function selectCountries() {
         {x: 1167, y: 408},
         {x: 1224, y: 392}
       ];
+      source = "Source: Wikipedia (UChicago AQLI 2022). https://en.wikipedia.org/wiki/List_of_countries_by_air_pollution";
       break;
     case 12:
       pinCoordinates = [
@@ -140,6 +153,7 @@ function selectCountries() {
         {x: 805, y: 198},
         {x: 335, y: 659}
       ];
+      source = "Source: Wikipedia (UChicago AQLI 2022). https://en.wikipedia.org/wiki/List_of_countries_by_air_pollution";
       break;
     // Add more cases for other options
     default:
