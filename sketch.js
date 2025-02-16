ac9f3dd2842cf9e800d414c39537c28310a56f2f
Valentina Fava <valentinafava93@gmail.com>
2025-02-16 01:42:11 -0300

    Added country name when hovering over a pin.

--- sketch.js
+++ sketch.js
@@ -40,143 +40,143 @@ function selectCountries() {
   switch (selectedOption) {
     case 0:
       pinCoordinates = [
-        {x: 11480 / originalImgWidth, y: 4890 / originalImgHeight},
-        {x: 12477 / originalImgWidth, y: 4220 / originalImgHeight},
-        {x: 2626 / originalImgWidth, y: 3950 / originalImgHeight},
-        {x: 13015 / originalImgWidth, y: 6550 / originalImgHeight},
-        {x: 10800 / originalImgWidth, y: 4450 / originalImgHeight}
+        {x: 11480 / originalImgWidth, y: 4890 / originalImgHeight, name: "1. India"},
+        {x: 12477 / originalImgWidth, y: 4220 / originalImgHeight, name: "2. China"},
+        {x: 2626 / originalImgWidth, y: 3950 / originalImgHeight, name: "3. United States"},
+        {x: 13015 / originalImgWidth, y: 6550 / originalImgHeight, name: "4. Indonesia"},
+        {x: 10800 / originalImgWidth, y: 4450 / originalImgHeight, name: "5. Pakistan"}
       ];
       link = "https://data.worldbank.org/indicator/SP.POP.TOTL?most_recent_value_desc=true";
       source = "Source: World Bank. ";
       break;
     case 1:
       pinCoordinates = [
-        {x: 8100 / originalImgWidth, y: 3730 / originalImgHeight},
-        {x: 7840 / originalImgWidth, y: 3575 / originalImgHeight},
-        {x: 7220 / originalImgWidth, y: 4007 / originalImgHeight},
-        {x: 15660 / originalImgWidth, y: 6250 / originalImgHeight},
-        {x: -70 / originalImgWidth, y: 6700 / originalImgHeight}
+        {x: 8100 / originalImgWidth, y: 3730 / originalImgHeight, name: "1. Vatican City"},
+        {x: 7840 / originalImgWidth, y: 3575 / originalImgHeight, name: "2. Monaco"},
+        {x: 7220 / originalImgWidth, y: 4007 / originalImgHeight, name: "3. Gibraltar"},
+        {x: 15660 / originalImgWidth, y: 6250 / originalImgHeight, name: "4. Tokelau"},
+        {x: -70 / originalImgWidth, y: 6700 / originalImgHeight, name: "5. Nauru"}
       ];
       link = "https://worldpopulationreview.com/country-rankings/smallest-countries";
       source = "Source: World Population Review. ";
       break;
     case 2:
       pinCoordinates = [
-        {x: 13190 / originalImgWidth, y: 4855 / originalImgHeight},
-        {x: 7825 / originalImgWidth, y: 3578 / originalImgHeight},
-        {x: 12830 / originalImgWidth, y: 6075 / originalImgHeight},
-        {x: 13230 / originalImgWidth, y: 4850 / originalImgHeight},
-        {x: 7220 / originalImgWidth, y: 4007 / originalImgHeight}
+        {x: 13190 / originalImgWidth, y: 4855 / originalImgHeight, name: "1. Macao"},
+        {x: 7825 / originalImgWidth, y: 3578 / originalImgHeight, name: "2. Monaco"},
+        {x: 12830 / originalImgWidth, y: 6075 / originalImgHeight, name: "3. Singapore"},
+        {x: 13230 / originalImgWidth, y: 4850 / originalImgHeight, name: "4. Hong Kong"},
+        {x: 7220 / originalImgWidth, y: 4007 / originalImgHeight, name: "5. Gibraltar"}
       ];
       link = "https://ourworldindata.org/most-densely-populated-countries";
       source = "Source: Our World in Data. ";
       break;
     case 3:
       pinCoordinates = [
-        {x: 6000 / originalImgWidth, y: 2200 / originalImgHeight},
-        {x: 4900 / originalImgWidth, y: 9190 / originalImgHeight},
-        {x: 6700 / originalImgWidth, y: 4800 / originalImgHeight},
-        {x: 12477 / originalImgWidth, y: 3400 / originalImgHeight},
-        {x: 8200 / originalImgWidth, y: 7560 / originalImgHeight}
+        {x: 6000 / originalImgWidth, y: 2200 / originalImgHeight, name: "1. Greenland"},
+        {x: 4900 / originalImgWidth, y: 9190 / originalImgHeight, name: "2. Falkland Islands"},
+        {x: 6700 / originalImgWidth, y: 4800 / originalImgHeight, name: "3. Western Sahara"},
+        {x: 12477 / originalImgWidth, y: 3400 / originalImgHeight, name: "4. Mongolia"},
+        {x: 8200 / originalImgWidth, y: 7560 / originalImgHeight, name: "5. Namibia"}
       ];
       link = "https://www.globalcitizensolutions.com/countries-with-lowest-population-density/";
       source = "Source: Global Citizen Solutions. ";
       break;
     case 4:
       pinCoordinates = [
-        {x: 2626 / originalImgWidth, y: 3950 / originalImgHeight},
-        {x: 12477 / originalImgWidth, y: 4220 / originalImgHeight},
-        {x: 14200 / originalImgWidth, y: 4080 / originalImgHeight},
-        {x: 7900 / originalImgWidth, y: 3205 / originalImgHeight},
-        {x: 7400 / originalImgWidth, y: 3160 / originalImgHeight}
+        {x: 2626 / originalImgWidth, y: 3950 / originalImgHeight, name: "1. United States"},
+        {x: 12477 / originalImgWidth, y: 4220 / originalImgHeight, name: "2. China"},
+        {x: 14200 / originalImgWidth, y: 4080 / originalImgHeight, name: "3. Japan"},
+        {x: 7900 / originalImgWidth, y: 3205 / originalImgHeight, name: "4. Germany"},
+        {x: 7400 / originalImgWidth, y: 3160 / originalImgHeight, name: "5. United Kingdom"}
       ];
       link = "https://en.wikipedia.org/wiki/List_of_countries_by_total_wealth#cite_note-databook2023-2";
       source = "Source: Wikipedia. ";
       break;
     case 5:
       pinCoordinates = [
-        {x: 7750 / originalImgWidth, y: 6200 / originalImgHeight},
-        {x: 4540 / originalImgWidth, y: 5880 / originalImgHeight},
-        {x: 8925 / originalImgWidth, y: 7805 / originalImgHeight},
-        {x: 6680 / originalImgWidth, y: 5440 / originalImgHeight},
-        {x: 9635 / originalImgWidth, y: 5445 / originalImgHeight}
+        {x: 7750 / originalImgWidth, y: 6200 / originalImgHeight, name: "1. São Tomé and Príncipe"},
+        {x: 4540 / originalImgWidth, y: 5880 / originalImgHeight, name: "2. Suriname"},
+        {x: 8925 / originalImgWidth, y: 7805 / originalImgHeight, name: "3. Lesotho"},
+        {x: 6680 / originalImgWidth, y: 5440 / originalImgHeight, name: "4. Guinea-Bissau"},
+        {x: 9635 / originalImgWidth, y: 5445 / originalImgHeight, name: "5. Djibouti"}
       ];
       link = "https://en.wikipedia.org/wiki/List_of_countries_by_total_wealth#cite_note-databook2023-2";
       source = "Source: Wikipedia. ";
       break;
     case 6:
       pinCoordinates = [
-        {x: 12830 / originalImgWidth, y: 6075 / originalImgHeight},
-        {x: 7805 / originalImgWidth, y: 3200 / originalImgHeight},
-        {x: 10045/ originalImgWidth, y: 4670 / originalImgHeight},
-        {x: 7160 / originalImgWidth, y: 3105 / originalImgHeight},
-        {x: 13190 / originalImgWidth, y: 4855 / originalImgHeight}
+        {x: 12830 / originalImgWidth, y: 6075 / originalImgHeight, name: "1. Singapore"},
+        {x: 7805 / originalImgWidth, y: 3200 / originalImgHeight, name: "2. Luxembourg"},
+        {x: 10045/ originalImgWidth, y: 4670 / originalImgHeight, name: "3. Qatar"},
+        {x: 7160 / originalImgWidth, y: 3105 / originalImgHeight, name: "4. Ireland"},
+        {x: 13190 / originalImgWidth, y: 4855 / originalImgHeight, name: "5. Macao"}
       ];
       link = "https://data.worldbank.org/indicator/NY.GDP.PCAP.PP.CD?most_recent_value_desc=true";
       source = "Source: World Bank. ";
       break;
     case 7:
       pinCoordinates = [
-        {x: 9000 / originalImgWidth, y: 6385 / originalImgHeight},
-        {x: 9005 / originalImgWidth, y: 5760 / originalImgHeight},
-        {x: 8600/ originalImgWidth, y: 5720 / originalImgHeight},
-        {x: 9890 / originalImgWidth, y: 5885 / originalImgHeight},
-        {x: 8600 / originalImgWidth, y: 6385 / originalImgHeight}
+        {x: 9000 / originalImgWidth, y: 6385 / originalImgHeight, name: "1. Burundi"},
+        {x: 9005 / originalImgWidth, y: 5760 / originalImgHeight, name: "2. South Sudan"},
+        {x: 8600/ originalImgWidth, y: 5720 / originalImgHeight, name: "3. Central African Republic"},
+        {x: 9890 / originalImgWidth, y: 5885 / originalImgHeight, name: "4. Somalia"},
+        {x: 8600 / originalImgWidth, y: 6385 / originalImgHeight, name: "5. Democratic Republic of the Congo"}
       ];
       link = "https://data.worldbank.org/indicator/NY.GDP.PCAP.PP.CD?most_recent_value_desc=true";
       source = "Source: World Bank. ";
       break;
     case 8:
       pinCoordinates = [
-        {x: 8450 / originalImgWidth, y: 3250 / originalImgHeight},
-        {x: 8228 / originalImgWidth, y: 3500 / originalImgHeight},
-        {x: 8870 / originalImgWidth, y: 3050 / originalImgHeight},
-        {x: 8895 / originalImgWidth, y: 3225 / originalImgHeight},
-        {x: 8870 / originalImgWidth, y: 3420 / originalImgHeight}
+        {x: 8450 / originalImgWidth, y: 3250 / originalImgHeight, name: "1. Slovak Republic"},
+        {x: 8228 / originalImgWidth, y: 3500 / originalImgHeight, name: "2. Slovenia"},
+        {x: 8870 / originalImgWidth, y: 3050 / originalImgHeight, name: "3. Belarus"},
+        {x: 8895 / originalImgWidth, y: 3225 / originalImgHeight, name: "4. Ukraine"},
+        {x: 8870 / originalImgWidth, y: 3420 / originalImgHeight, name: "5. Moldova"}
       ];
       link = "https://data.worldbank.org/indicator/SI.POV.GINI?most_recent_value_desc=false";
       source = "Source: World Bank. ";
       break;
     case 9:
       pinCoordinates = [
-        {x: 8600 / originalImgWidth, y: 8000 / originalImgHeight},
-        {x: 8280 / originalImgWidth, y: 7500 / originalImgHeight},
-        {x: 3585 / originalImgWidth, y: 5900 / originalImgHeight},
-        {x: 9040 / originalImgWidth, y: 7720 / originalImgHeight},
-        {x: 8600 / originalImgWidth, y: 7500 / originalImgHeight}
+        {x: 8600 / originalImgWidth, y: 8000 / originalImgHeight, name: "1. South Africa"},
+        {x: 8280 / originalImgWidth, y: 7500 / originalImgHeight, name: "2. Namibia"},
+        {x: 3585 / originalImgWidth, y: 5900 / originalImgHeight, name: "3. Colombia"},
+        {x: 9040 / originalImgWidth, y: 7720 / originalImgHeight, name: "4. Eswatini"},
+        {x: 8600 / originalImgWidth, y: 7500 / originalImgHeight, name: "5. Botswana"}
       ];
       link = "https://data.worldbank.org/indicator/SI.POV.GINI?most_recent_value_desc=false";
       source = "Source: World Bank. ";
       break;
     case 10:
       pinCoordinates = [
-        {x: 14880 / originalImgWidth, y: 6550 / originalImgHeight},
-        {x: 13015 / originalImgWidth, y: 6550 / originalImgHeight},
-        {x: 7800 / originalImgWidth, y: 5680 / originalImgHeight},
-        {x: 11480 / originalImgWidth, y: 4890 / originalImgHeight},
-        {x: 2626 / originalImgWidth, y: 3950 / originalImgHeight}
+        {x: 14880 / originalImgWidth, y: 6550 / originalImgHeight, name: "1. Papa New Guinea"},
+        {x: 13015 / originalImgWidth, y: 6550 / originalImgHeight, name: "2. Indonesia"},
+        {x: 7800 / originalImgWidth, y: 5680 / originalImgHeight, name: "3. Nigeria"},
+        {x: 11480 / originalImgWidth, y: 4890 / originalImgHeight, name: "4. India"},
+        {x: 2626 / originalImgWidth, y: 3950 / originalImgHeight, name: "5. United States"}
       ];
       link = "https://www.weforum.org/stories/2021/03/these-are-the-top-ten-countries-for-linguistic-diversity/";
       source = "Source: We Forum. ";
       break;
     case 11:
       pinCoordinates = [
-        {x: 12000 / originalImgWidth, y: 4800 / originalImgHeight},
-        {x: 11480 / originalImgWidth, y: 4890 / originalImgHeight},
-        {x: 11720 / originalImgWidth, y: 4450 / originalImgHeight},
-        {x: 10045 / originalImgWidth, y: 4670 / originalImgHeight},
-        {x: 10800 / originalImgWidth, y: 4450 / originalImgHeight}
+        {x: 12000 / originalImgWidth, y: 4800 / originalImgHeight, name: "1. Bangladesh"},
+        {x: 11480 / originalImgWidth, y: 4890 / originalImgHeight, name: "2. India"},
+        {x: 11720 / originalImgWidth, y: 4450 / originalImgHeight, name: "3. Nepal"},
+        {x: 10045 / originalImgWidth, y: 4670 / originalImgHeight, name: "4. Qatar"},
+        {x: 10800 / originalImgWidth, y: 4450 / originalImgHeight, name: "5. Pakistan"}
       ];
       link = "https://en.wikipedia.org/wiki/List_of_countries_by_air_pollution";
       source = "Source: Wikipedia (UChicago AQLI 2022). ";
       break;
     case 12:
       pinCoordinates = [
-        {x: 925 / originalImgWidth, y: 6200 / originalImgHeight},
-        {x: 15425 / originalImgWidth, y: 5820 / originalImgHeight},
-        {x: 15600 / originalImgWidth, y: 5820 / originalImgHeight},
-        {x: 6000 / originalImgWidth, y: 2200 / originalImgHeight},
-        {x: 200 / originalImgWidth, y: 7670 / originalImgHeight}
+        {x: 925 / originalImgWidth, y: 6200 / originalImgHeight, name: "1. Kiribati"},
+        {x: 15425 / originalImgWidth, y: 5820 / originalImgHeight, name: "2. Federal States of Micronesia"},
+        {x: 15600 / originalImgWidth, y: 5820 / originalImgHeight, name: "3. Marshall Islands"},
+        {x: 6000 / originalImgWidth, y: 2200 / originalImgHeight, name: "4. Greenland"},
+        {x: 200 / originalImgWidth, y: 7670 / originalImgHeight, name: "5. Cook Islands"}
       ];
       link = "https://en.wikipedia.org/wiki/List_of_countries_by_air_pollution";
       source = "Source: Wikipedia (UChicago AQLI 2022). ";
@@ -216,6 +216,14 @@ function draw() {
     let pinX = x + pin.x * imgWidth;
     let pinY = y + pin.y * imgHeight;
     image(img2, pinX, pinY, 14.3, 30);
+
+    // Check if the mouse is over the pin
+    let distance = dist(mouseX, mouseY, pinX, pinY);
+    if (distance < 15) {
+      fill(0);
+      textSize(18);
+      text(pin.name, mouseX + 10, mouseY - 10);
+    }
   }
 
   fill(0);
